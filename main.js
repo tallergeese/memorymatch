13869fd314e78579aab37ff05b6ba6e32a11fd31
Michael Duong <Michael Duong>
2017-01-20 00:12:00 -0800

    memory match 1.0 - Michael Duong

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 //Global Variables
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
 
 var attempts = 0;
@@ -13,6 +13,7 @@ var accuracy = 0;
 var games_played = 0;
 
 $(document).ready(function(){
+    cardRandomizer();
     $(".card").click(card_clicked);
     $(".reset").click(function(){
         reset_stats();
@@ -30,42 +31,43 @@ function card_clicked(){
     $(this).find(".back").hide();
 
     if (first_card_clicked == null){
-         first_card_clicked = $(this).find("img").attr("src");
+         first_card_clicked = $(this);
          $(this).addClass("already_clicked");
     }
      else {
-         second_card_clicked = $(this).find("img").attr("src");
+         second_card_clicked = $(this);
          $(this).addClass("already_clicked");
 
          attempts++;
-         console.log("attempts", attempts);
          display_stats();
 
-         if (first_card_clicked == second_card_clicked) {
+         var first_card_compare = first_card_clicked.find("img").attr("src");
+         var second_card_compare = second_card_clicked.find("img").attr("src");
+
+         if (first_card_compare.substr(0,3) === second_card_compare.substr(0,3)) {
+             console.log(first_card_clicked.find("img").attr("src"));
              first_card_clicked = null;
              second_card_clicked = null;
 
              match_counter++;
              display_stats();
-             console.log("match_counter", match_counter);
-
 
              if (match_counter >= total_possible_matches) {
-                 setTimeout(function () {
-                     alert("You won!");
-                 });
+                 setTimeout(winnerIsYou);
              }
          }
 
          else {
+             $(".card").off("click");
              setTimeout(cardsGoFacedown, 2000);
-             $(".card").off();
+
          }
     }
 }
 
 function cardsGoFacedown() {
-    $(".back").show();
+    first_card_clicked.find(".back").show();
+    second_card_clicked.find(".back").show();
     $(".card").click(card_clicked).removeClass("already_clicked");
     first_card_clicked = null;
     second_card_clicked = null;
@@ -75,7 +77,7 @@ function display_stats(){
     $(".games-played .value").text(games_played);
     $(".attempts .value").text(attempts);
     if (attempts != 0 || match_counter != 0) {
-        accuracy = (match_counter/attempts * 100) + "%";
+        accuracy = Math.floor((match_counter/attempts * 10000))/100 + "%";
         $(".accuracy .value").text(accuracy);
     }
     else{
@@ -83,11 +85,45 @@ function display_stats(){
     }
 }
 
+function winnerIsYou(){
+    var $winner = $("<div>").addClass("winner").text("YOU WON!");
+    var $reset = $("<button>").addClass("reset").text("TRY AGAIN?");
+    $("body").append($winner);
+    $(".winner").append($reset);
+    $(".winner .reset").click(function(){
+        reset_stats();
+        display_stats();
+    });
+
+}
+
 function reset_stats(){
+    $(".winner").remove();
     accuracy = 0;
     match_counter = 0;
     attempts = 0;
     games_played++;
-    cardsGoFacedown();
+
+    $(".back").show();
+    $(".card").click(card_clicked).removeClass("already_clicked");
+    first_card_clicked = null;
+    second_card_clicked = null;
+
     display_stats();
+    cardRandomizer();
 }
+
+//RANDOMIZER
+function getRandomNumber(min, max){
+    return Math.floor(Math.random()*(max-min+1)) + min;
+}
+
+function cardRandomizer(){
+    for (var i = 0; i < 100; i++){
+        var random = getRandomNumber(1,18);
+        var randomizedCard = $(".card:nth-child(" + random + ")");
+        $("#game-area").append(randomizedCard);
+    }
+}
+
+//
